fe96538855f13063a37b5f8a1e115e8670b9c5ef
charan-churu <92526595+charan-churu@users.noreply.github.com>
2024-02-09 11:13:40 +0530

    Add files via upload

---
+++ src/components/Team.js
@@ -0,0 +1,164 @@
+import React ,{useRef,useState}from 'react'
+import './Team.css'
+
+function Team() {
+  return (
+    <div className='body'>
+      
+      <header className='head'>
+        <h1>Company Name</h1>
+        
+      </header>
+
+      <article>
+      <div className='team'><b>Meet Our Team</b></div>
+
+      <h2 className='title'>Leadership / Management Team</h2>
+
+      <section className='section'>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Founder Name</p>
+            <i>founder/ CEO</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Founder Name</p>
+            <i>founder/ CEO</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1"/>
+            <p>VP Name</p>
+            <i>VP, Development & Growth</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>VP Name</p>
+            <i>VP, sales & Marketing</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Founder Name</p>
+            <i>VP finance & Operations</i>
+        </div>
+      </section>
+
+      <h2 className='title'>Development Team</h2>
+
+      <section className='section'>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Developer Name</p>
+            <i>Team Lead, SE</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1"/>
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Developer Name</p>
+            <i>Software Engineer</i>
+        </div>
+      </section>
+      
+      <h2 className='title'>Sales and Marketing Team</h2>
+      
+      <section className='section'  >
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1"/>
+            <p>Employee Name</p>
+            <i>Team Lead</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Employee Name</p>
+            <i>Software Engineer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Developer Name</p>
+            <i>Sales Officer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Developer Name</p>
+            <i>Sales Officer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Founder Name</p>
+            <i>Marketing Officer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Founder Name</p>
+            <i>Marketing Officer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Developer Name</p>
+            <i>Marketing Officer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Founder Name</p>
+            <i>Marketing Officer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/w3css/img_avatar.png" alt="Person1" />
+            <p>Developer Name</p>
+            <i>Marketing Officer</i>
+        </div>
+        <div className="container">
+            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Person2" />
+            <p>Founder Name</p>
+            <i>Marketing Officer</i>
+        </div>
+      </section>
+      </article>
+      <footer className="footer">
+        <p>&copy; 2024 Company Name. All Rights Reserved.</p>
+      </footer>
+        
+    </div>
+  )
+}
+
+export default Team
